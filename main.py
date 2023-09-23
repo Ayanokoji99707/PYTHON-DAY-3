1893746e55a6101f8fc6994d47b0db5a5fcfebd2
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-23 14:50:19 +0600

    TEST

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
 
-if height >= 120:
-    print("You can go")
-else:
-    print("You can not go bro")
-## STEP COMPLET
+# if height != 120:
+#     print("You can go")
+# else:
+#     print("You can not go bro")
+
+# x = int(input("What Number do you want to check ?"))
+# x = x / 2
+# print(x)
+# if x 
+#     print("This is even number")
+# else:
+#     print("This is oda number")
+
