b408988d2af5bb83ac2513b98fc7555f39689a49
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-23 15:39:55 +0600

    Update
    ok

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-# print("Welcome to the rollercoaster!")
-# height = int(input("What is your height in cm? "))
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
 
-# if height != 120:
-#     print("You can go")
-# else:
-#     print("You can not go bro")
-
-number = int(input("Which number do you want to check? "))
-number %= 2
-if number == 1:
-    print("This is an odd number.")
+if height != 120:
+    print("You can go")
 else:
-    print("This is an even number.")
+    print("You can not go bro")
+
+x = int(input("What Number do you want to check ?"))
+x %= 2
+if x == 1:
+    print("This is oda number")
+else:
+    print("This is even number")
+
+height = int(input("What is your Height? "))
+
+if height >= 120:
+    print("Go TO Railway")
+    age = int(input("what is your age? "))
+    if age >= 18:
+        print("Your Ticket Price is $12")
+    elif age >= 12:
+        print("your Ticket is $7")
+    elif age < 12:
+        print("Your Ticket price is $5")
+    else:
+        print("You can not go to railway")
+else:
+    print("Sorry, you should not go")
