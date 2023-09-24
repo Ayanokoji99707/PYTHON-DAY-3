3f89fe114bf73f5e82938813e807e391490cc3a0
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-24 23:59:03 +0600

    WRONG

--- leepyear.py
+++ leepyear.py
@@ -6,4 +6,13 @@
 #     print("Not working")
 
 #     ############ EPLOAD COSE ///
+year=input("which year you want to check?") 
 
+if year<1700:
+ print("YOU ARE WELCOME")
+elif year/4:
+ print("you are right")
+enen:
+ print("You are not")
+even:
+ print("you are not")
