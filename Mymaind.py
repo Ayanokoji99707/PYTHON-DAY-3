aaaba4a49413110af3a9f1675ad35f2821e41d26
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-26 19:46:48 +0600

    Update Mymaind.py

--- Mymaind.py
+++ Mymaind.py
@@ -7,6 +7,17 @@
 
 year = int(input("Which year do you want to check? "))
 
+# if (year / 4) == int(year / 4):
+#     if (year / 100) == int(year / 100):
+#         if (year / 400) == int(year / 400):
+#             print("Leap year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
+
 if year % 4:
     if year % 100:
         if year % 400:
