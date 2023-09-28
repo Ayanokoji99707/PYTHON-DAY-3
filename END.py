7525a0f807a1e86c409251762a1f4eb732065c9e
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-28 19:54:57 +0600

    Update END.py

---
+++ END.py
@@ -0,0 +1,6 @@
+print("I can do it")
+print("Why i am not doing this think")
+print("Fuck it , I want to do it")
+print("What Is this ")
+print("I can realy do it")
+print("Hey, Where are you go Maruf")
